d3f3fc6f5598f66785d984b6bffe4b636b5ec5c7
akdcls1 <56528690+akdcls1@users.noreply.github.com>
2023-10-28 23:19:04 +0900

    section_3_40
    section_3_40 complete

--- Snow Boader/Assets/Script/CrashDetector.cs
+++ Snow Boader/Assets/Script/CrashDetector.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    [SerializeField] float loadDelay = 1f;
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", loadDelay);
         }
     }
