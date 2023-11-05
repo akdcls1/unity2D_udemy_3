1acd4a8033e6d4b8ef224c65af94008ab7ed6ea6
akdcls1 <56528690+akdcls1@users.noreply.github.com>
2023-11-06 00:30:33 +0900

    section_3_45_complete
    section_3_45_complete
    audio source add

--- Snow Boader/Assets/Script/FinishLine.cs
+++ Snow Boader/Assets/Script/FinishLine.cs
@@ -7,11 +7,13 @@ public class FinishLine : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
     [SerializeField] ParticleSystem finishEffect;
+
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
         {
             finishEffect.Play();
+            GetComponent<AudioSource>().Play();
             Invoke("ReloadScene", loadDelay);
         }
     }
@@ -20,4 +22,6 @@ public class FinishLine : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+
 }
